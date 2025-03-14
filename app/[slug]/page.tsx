8bb85ddc06ec6de451204c56ac1c0dc61bf82ab6
Tyler R <tyler73750@gmail.com>
2025-03-14 14:33:34 -0500

    fix(page.tsx): add a missing semicolon and remove unnecessary spacing

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getPostBySlug, getSortedPostsData } from '@/libs/posts.ts';
 
 // Return a 404 if the user visits a site not pre-generated
-export const dynamicParams = false
+export const dynamicParams = false;
 
 // Generate dynamic paths at buildtime
 export async function generateStaticParams() {
@@ -12,25 +12,28 @@ export async function generateStaticParams() {
 export default async function BlogPost({
   params,
 }: {
-    params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
   const post = await getPostBySlug(slug);
+  console.log(post.contentHtml);
   return (
     <div className="flex min-h-screen flex-col bg-stone-100 p-6 text-gray-900">
-      <main className="max-w-3xl mx-auto w-full">
+      <main className="mx-auto w-full max-w-3xl">
         <header className="border-b-2 border-emerald-700 pb-4">
           <h1>{post.title}</h1>
-          <p>Last Updated: { post.date }</p>
+          <p>Last Updated: {post.date}</p>
           <p>
-            <a href="https://github.com/ROMANT21/commit-suggestions"
-             className="text-emerald-700 hover:text-emerald-900 underline">
-                Link to codebase
-             </a>
+            <a
+              href="https://github.com/ROMANT21/commit-suggestions"
+              className="text-emerald-700 underline hover:text-emerald-900"
+            >
+              Link to codebase
+            </a>
           </p>
         </header>
         <div
-          className="prose prose-stone pt-4"
+          className="min-w-full space-y-4 pt-4"
           dangerouslySetInnerHTML={{ __html: post.contentHtml }}
         ></div>
       </main>
